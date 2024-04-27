2ca1a8fc859723b49d3abeda5b77b591ab16e577
Raphaelgruet <92589374+Raphaelgruet@users.noreply.github.com>
2024-04-27 20:59:34 +0200

    Update NoteDao.kt
    Correction des fautes dans NoteDao

--- app/src/main/java/com/eseo/noteapp/model/dao/NoteDao.kt
+++ app/src/main/java/com/eseo/noteapp/model/dao/NoteDao.kt
@@ -12,19 +12,19 @@ import com.eseo.noteapp.model.entity.Note
  de données SQLite. Elle définit un ensemble de méthodes qui permettent à votre application
  d'effectuer des opérations CRUD (Create, Read, Update, Delete)
 */
-//@Dao permet de déclaré cette interface comme un Data Access Object.
+//@Dao permet de déclarer cette interface comme un Data Access Object.
 @Dao
 interface NoteDao {
-    /*permet de dire que la méthode permet d'inserer dans la base de donné des donné sous forme Note*/
+    /*permet de dire que la méthode permet d'inserer dans la base de données des données sous forme Note*/
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNote(note: Note)
-    /*permet de mettre les donné de la base de donné dans une liste de type note*/
+    /*permet de mettre les données de la base de données dans une liste de type note*/
     @Query("SELECT * FROM note")
     fun getAllNotes() : LiveData<List<Note>>
-    /*permet de supprimer une note dans la base de donné*/
+    /*permet de supprimer une note dans la base de données*/
     @Query("DELETE FROM note WHERE id = :id")
     suspend fun deleteNote(id : Int)
-    /*permet de mettre a jour une note dans la base de donné*/
+    /*permet de mettre à jour une note dans la base de données*/
     @Update(
         entity = Note::class,
         onConflict = OnConflictStrategy.REPLACE
